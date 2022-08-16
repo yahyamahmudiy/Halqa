7d1c9944734ba6aabbed8b70844ac6b215b7b77c
Yahya Mahmudiy <mustofamahmudiy@gmail.com>
2022-08-16 07:50:04 +0500

    Select Language

--- app/src/main/java/com/example/halqa/MainActivity.kt
+++ app/src/main/java/com/example/halqa/MainActivity.kt
@@ -1,7 +1,7 @@
 package com.example.halqa
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import com.example.halqa.databinding.ActivityMainBinding
--- app/src/main/java/com/example/halqa/fragment/languageflow/language/LanguageFragment.kt
+++ app/src/main/java/com/example/halqa/fragment/languageflow/language/LanguageFragment.kt
@@ -1,9 +1,54 @@
 package com.example.halqa.fragment.languageflow.language
 
+import android.os.Bundle
+import android.view.View
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
+import by.kirich1409.viewbindingdelegate.viewBinding
 import com.example.halqa.R
-import com.example.halqa.fragment.BaseFragment
+import com.example.halqa.databinding.FragmentLanguageBinding
 
 class LanguageFragment : Fragment(R.layout.fragment_language) {
+    private val binding by viewBinding(FragmentLanguageBinding::bind)
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initViews()
+    }
+
+    private fun initViews() {
+        selectLanguage()
+    }
+
+    fun selectLanguage() {
+        binding.apply {
+            btnLotin.setOnClickListener {
+                btnLotin.background =
+                    ContextCompat.getDrawable(requireContext(), R.drawable.bg_button_blue)
+                btnLotin.setTextColor(resources.getColor(R.color.white))
+
+                btnKirill.background =
+                    ContextCompat.getDrawable(requireContext(), R.drawable.bg_button_white)
+                btnKirill.setTextColor(resources.getColor(R.color.black))
+
+                btnContinue.background =
+                    ContextCompat.getDrawable(requireContext(), R.drawable.bg_button_blue)
+                btnContinue.setTextColor(resources.getColor(R.color.white))
+            }
+
+            btnKirill.setOnClickListener {
+                btnKirill.background =
+                    ContextCompat.getDrawable(requireContext(), R.drawable.bg_button_blue)
+                btnKirill.setTextColor(resources.getColor(R.color.white))
+
+                btnLotin.background =
+                    ContextCompat.getDrawable(requireContext(), R.drawable.bg_button_white)
+                btnLotin.setTextColor(resources.getColor(R.color.black))
+
+                btnContinue.background =
+                    ContextCompat.getDrawable(requireContext(), R.drawable.bg_button_blue)
+                btnContinue.setTextColor(resources.getColor(R.color.white))
+            }
+        }
+    }
 }
