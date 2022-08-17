41a0a70cc76e877ab91ddcc5ad738cc8e7c9c5b4
islombeknasriddinov <nasriddinov.islom.19@gmail.com>
2022-08-17 16:46:24 +0500

    added audioBM and booksBM fragments

--- app/src/main/java/com/example/halqa/fragment/mainflow/bookmark/BookmarkFragment.kt
+++ app/src/main/java/com/example/halqa/fragment/mainflow/bookmark/BookmarkFragment.kt
@@ -1,8 +1,12 @@
 package com.example.halqa.fragment.mainflow.bookmark
 
 import androidx.fragment.app.Fragment
+import by.kirich1409.viewbindingdelegate.viewBinding
 import com.example.halqa.R
+import com.example.halqa.databinding.FragmentKitoblarBinding
+import com.example.halqa.databinding.FragmentLanguageBinding
 
 class BookmarkFragment : Fragment(R.layout.fragment_bookmark) {
+    private val binding by viewBinding(FragmentKitoblarBinding::bind)
 
 }
---
+++ app/src/main/java/com/example/halqa/fragment/mainflow/bookmark/audio/AudioBMFragment.kt
@@ -0,0 +1,11 @@
+package com.example.halqa.fragment.mainflow.bookmark.audio
+
+import androidx.fragment.app.Fragment
+import by.kirich1409.viewbindingdelegate.viewBinding
+import com.example.halqa.R
+import com.example.halqa.databinding.FragmentAudioBinding
+
+class AudioBMFragment : Fragment(R.layout.fragment_audio) {
+    private val binding by viewBinding(FragmentAudioBinding::bind)
+
+}
